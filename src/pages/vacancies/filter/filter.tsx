9f2a6da7f606f7f7c2aa1d415a90a994705f6525
YuryK96 <kast2406@gmail.com>
2023-05-05 22:32:26 +0300

    added salary in Filter

---
+++ src/pages/vacancies/filter/filter.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import s from './vacancies.module.scss'
+
+export const Vacancies: FC = () => {
+
+
+    return <section className={s.vacancies}>
+
+
+    </section>
+}
---
+++ src/pages/vacancies/search-panel/search-panel.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import s from './filter.module.scss'
+
+export const Filter: FC = () => {
+
+
+    return <aside className={s.filter}>
+        
+
+    </aside>
+}
