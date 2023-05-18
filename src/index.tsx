89ff94b8449c6289755d2f1898834d66ac936a91
YuryK96 <kast2406@gmail.com>
2023-05-18 22:25:05 +0300

    added gh-pages and published project

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename="/test_Jobored">
                 <Routes>
                     <Route path='/' element={<Layout/>}>
                         <Route path='/' element={<Vacancies/>}/>
