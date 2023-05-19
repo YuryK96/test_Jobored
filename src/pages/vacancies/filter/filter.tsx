f905f4ff49cb065bb27adb9d7b9855f46b82815d
YuryK96 <kast2406@gmail.com>
2023-05-19 21:06:14 +0300

    markup for mobile

--- src/pages/vacancies/filter/filter.tsx
+++ src/pages/vacancies/filter/filter.tsx
@@ -20,9 +20,14 @@ export const Filter: FC<FilterType> = ({
     const [isOpenList, setIsOpenList] = useState<boolean>(false)
     const categories = useSelector(getCategoryNamesKeysSelector)
     const [industry, setIndustry] = useState<string>('')
+    const [isOpenFilter, setIsOpenFilter] = useState<boolean>(true)
 
+    const toggleShowingFilter = () => {
+        setIsOpenFilter(!isOpenFilter)
+    }
 
-    const chooseIndustry = (industry: string) => {
+        const
+    chooseIndustry = (industry: string) => {
         setIndustry(industry)
     }
 
@@ -51,79 +56,82 @@ export const Filter: FC<FilterType> = ({
 
     }
 
-    return <aside className={s.filter}>
-        <div className={s.title}><h1>Фильтры</h1>
-            <div onClick={filterFieldsReset} className={s.reset}><span>Сбросить все</span> <img src={cross}
-                                                                                                alt='cross'/></div>
-        </div>
-        <div className={s.industry}>
-            <h2>Отрасль</h2>
+    return <aside className={s.filter_container}>
+        <button style={ {background: isOpenFilter ? '#ACADB9' : '' } } type='button' onClick={toggleShowingFilter} className={s.show_filter_btn}>{ isOpenFilter ? 'Скрыть фильтр' : 'Показать фильтр' }</button>
+        <div className={`${s.filter} ${isOpenFilter ? '' : s.closeFilter}`}>
+            <div className={s.title}><h1>Фильтры</h1>
+                <div onClick={filterFieldsReset} className={s.reset}><span>Сбросить все</span> <img src={cross}
+                                                                                                    alt='cross'/></div>
+            </div>
+            <div className={s.industry}>
+                <h2>Отрасль</h2>
 
-            <div onClick={toggleList}  data-elem="industry-select"
-                 className={`${s.industry_input} ${industry ? s.blackText : s.grayText}  ${isOpenList ? s.blueBorder : ''}`}>
+                <div onClick={toggleList} data-elem="industry-select"
+                     className={`${s.industry_input} ${industry ? s.blackText : s.grayText}  ${isOpenList ? s.blueBorder : ''}`}>
                 <span
                     style={{color: industry ? "#232134FF" : "#ACADB9FF"}}>{industry ? industry : 'Выберите отрасль'} </span>
-                <img src={arrow_down} alt='arrow'/>
-                <div className={`${s.list} ${isOpenList ? s.openList : ''}`}>
-                    {categories.map((category) => {
-                        return <span key={category.key} onClick={() => {
-                            chooseIndustry(category.title);
-                            chooseIndustryKey(category.key)
-                        }}>{category.title}</span>
-                    })}
+                    <img src={arrow_down} alt='arrow'/>
+                    <div className={`${s.list} ${isOpenList ? s.openList : ''}`}>
+                        {categories.map((category) => {
+                            return <span key={category.key} onClick={() => {
+                                chooseIndustry(category.title);
+                                chooseIndustryKey(category.key)
+                            }}>{category.title}</span>
+                        })}
+                    </div>
                 </div>
+
             </div>
 
-        </div>
-
-        <div className={s.salary}>
-            <h2>Оклад</h2>
+            <div className={s.salary}>
+                <h2>Оклад</h2>
 
 
-            <div className={s.input_container}><input
-                data-elem="salary-from-input"
-                onKeyDown={(evt: React.KeyboardEvent<HTMLInputElement>) => ["e", "E", "+", "-","."].includes(evt.key) && evt.preventDefault()}
-                {...register('numberFrom', {
-                    validate: {
-                        correctNumber: (value: string, formValues) => {
+                <div className={s.input_container}><input
+                    data-elem="salary-from-input"
+                    onKeyDown={(evt: React.KeyboardEvent<HTMLInputElement>) => ["e", "E", "+", "-", "."].includes(evt.key) && evt.preventDefault()}
+                    {...register('numberFrom', {
+                        validate: {
+                            correctNumber: (value: string, formValues) => {
 
-                            return Number(value) < Number(formValues.numberUpTo) || !formValues.numberUpTo || !value || 'оклад ОТ не может быть меньше оклада ДО'
+                                return Number(value) < Number(formValues.numberUpTo) || !formValues.numberUpTo || !value || 'оклад ОТ не может быть меньше оклада ДО'
+                            }
                         }
-                    }
-                })} type='number' placeholder='От'/>
-                <div className={s.arrows_container}>
-                    <div className={s.salaryArrow_up}><img onClick={() => salaryIncrement('numberFrom')}
-                                                           src={arrow_up_salary} alt='arrow_up_salary'/></div>
-                    <div className={s.salaryArrow_down}><img onClick={() => salaryDecrement('numberFrom')}
-                                                             src={arrow_down_salary} alt='arrow_down_salary'/></div>
+                    })} type='number' placeholder='От'/>
+                    <div className={s.arrows_container}>
+                        <div className={s.salaryArrow_up}><img onClick={() => salaryIncrement('numberFrom')}
+                                                               src={arrow_up_salary} alt='arrow_up_salary'/></div>
+                        <div className={s.salaryArrow_down}><img onClick={() => salaryDecrement('numberFrom')}
+                                                                 src={arrow_down_salary} alt='arrow_down_salary'/></div>
+                    </div>
                 </div>
-            </div>
 
 
-            <div className={s.input_container}><input
-                data-elem="salary-to-input"
-                onKeyDown={(evt: React.KeyboardEvent<HTMLInputElement>) => ["e", "E", "+", "-","."].includes(evt.key) && evt.preventDefault()}
-                {...register('numberUpTo', {
-                    validate: {
-                        correctNumber: (value: string, formValues) => {
+                <div className={s.input_container}><input
+                    data-elem="salary-to-input"
+                    onKeyDown={(evt: React.KeyboardEvent<HTMLInputElement>) => ["e", "E", "+", "-", "."].includes(evt.key) && evt.preventDefault()}
+                    {...register('numberUpTo', {
+                        validate: {
+                            correctNumber: (value: string, formValues) => {
 
-                            return Number(value) > Number(formValues.numberFrom) || !formValues.numberFrom || !value || 'оклад ДО не может быть меньше оклада ОТ'
+                                return Number(value) > Number(formValues.numberFrom) || !formValues.numberFrom || !value || 'оклад ДО не может быть меньше оклада ОТ'
 
+                            }
                         }
-                    }
-                })} type='number' placeholder='До'/>
-                <div className={s.arrows_container}>
-                    <div className={s.salaryArrow_up}><img onClick={() => salaryIncrement('numberUpTo')}
-                                                           src={arrow_up_salary} alt='arrow_up_salary'/></div>
-                    <div className={s.salaryArrow_down}><img onClick={() => salaryDecrement('numberUpTo')}
-                                                             src={arrow_down_salary} alt='arrow_down_salary'/></div>
+                    })} type='number' placeholder='До'/>
+                    <div className={s.arrows_container}>
+                        <div className={s.salaryArrow_up}><img onClick={() => salaryIncrement('numberUpTo')}
+                                                               src={arrow_up_salary} alt='arrow_up_salary'/></div>
+                        <div className={s.salaryArrow_down}><img onClick={() => salaryDecrement('numberUpTo')}
+                                                                 src={arrow_down_salary} alt='arrow_down_salary'/></div>
+                    </div>
                 </div>
+
             </div>
 
+            <div className={s.error}> {!isValid && <span>Неверный диапазон сумм</span>}</div>
+            <button data-elem="search-button" className={s.button} type='submit'>Применить</button>
         </div>
-
-        <div className={s.error}> {!isValid && <span>Неверный диапазон сумм</span>}</div>
-        <button data-elem="search-button" className={s.button} type='submit'>Применить</button>
     </aside>
 }
 
